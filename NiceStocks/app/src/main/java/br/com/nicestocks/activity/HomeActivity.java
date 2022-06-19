777c9a510fa552cc83bbe420e915ff275a9177a2
mjigormj <igorcruzsilva17@gmail.com>
2022-06-18 21:57:10 -0300

    first commit

---
+++ NiceStocks/app/src/main/java/br/com/nicestocks/activity/HomeActivity.java
@@ -0,0 +1,16 @@
+package br.com.nicestocks.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.nicestocks.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ NiceStocks/app/src/main/java/br/com/nicestocks/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package br.com.nicestocks.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.nicestocks.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
